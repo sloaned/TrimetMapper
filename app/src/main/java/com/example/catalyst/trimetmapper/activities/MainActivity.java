4fa21f391bb898bd4fe23a97f3aa504777f2db8d
Daniel Sloane <dsloane@catalystdevworks.com>
2016-07-14 16:58:55 -0700

    autocomplete widget

--- app/src/main/java/com/example/catalyst/trimetmapper/activities/MainActivity.java
+++ app/src/main/java/com/example/catalyst/trimetmapper/activities/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.catalyst.trimetmapper.activities;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.example.catalyst.trimetmapper.R;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -30,24 +38,41 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
-   // @Bind(R.id.focus_layout) LinearLayout focus;
+    private Context mContext;
+
+    private int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+
+
+    @Bind(R.id.focus_layout) LinearLayout focus;
+    @Bind(R.id.destination_prompt) EditText searchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       // ButterKnife.bind(this);
+        ButterKnife.bind(this);
+
+        mContext = this;
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+       // PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
 
         LatLngBounds bounds = new LatLngBounds(new LatLng(44.6368, -124), new LatLng(46.9754, -121));
-        autocompleteFragment.setBoundsBias(bounds);
 
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+        searchBar.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (b) {
+                    openPlaceAutocomplete();
+                }
+            }
+        });
+       // autocompleteFragment.setBoundsBias(bounds);
+
+      /*  autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
                 Log.i(TAG, "Place: " + place.getName());
@@ -57,7 +82,36 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             public void onError(Status status) {
                 Log.i(TAG, "An error occurred: " + status);
             }
-        });
+        }); */
+    }
+
+    public void openPlaceAutocomplete() {
+        try {
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN).build(this);
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                Log.i(TAG, "Place: " + place.getName() + " " + place.getAddress() + " " + place.getLatLng());
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                // TODO: Handle the error.
+                Log.i(TAG, status.getStatusMessage());
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
     }
 
     @Override
